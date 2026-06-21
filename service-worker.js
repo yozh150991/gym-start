4516506fde05600c46c787bde01ae0ba2c250061
yozh150991 <49879938+yozh150991@users.noreply.github.com>
2026-06-21 23:39:26 +0200

    start of the project

---
+++ service-worker.js
@@ -0,0 +1,10 @@
+const CACHE='start-u-zali-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{
+    const cp=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));return resp;
+  }).catch(()=>caches.match('./index.html'))));
+});
