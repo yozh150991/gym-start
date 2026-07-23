6b958451ef9d4a3ebeb66c2c5fb40c0788b923b0
yozh150991 <49879938+yozh150991@users.noreply.github.com>
2026-07-24 00:30:47 +0200

    fixed adding personal exercises

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='start-u-zali-v12';
+const CACHE='start-u-zali-v15';
 const ASSETS=['./','./index.html','./config.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
